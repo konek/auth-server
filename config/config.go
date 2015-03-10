8d7c4d17a2ba411533b3467761f7d903da5a3208
yazou <yazgazan@gmail.com>
2015-03-10 17:13:52 -0400

    removing mongodb username and password from config as they are included in the MONGO_URL variable

--- config/config.go
+++ config/config.go
@@ -13,8 +13,6 @@ type Conf struct {
 	Listen            string
 	DbURL             string
 	DbName            string
-	DbUserName        string
-	DbPwd             string
 	SessionLifespan   int64
 	PasswordMinLength int
 	Root              string
@@ -27,8 +25,6 @@ const (
 	defaultListen            = ":8080"
 	defaultDbURL             = "localhost:27017"
 	defaultDbName            = "auth"
-	defaultDbUserName        = ""
-	defaultDbPwd             = ""
 	defaultSessionLifespan   = 60 * 60 * 24 // 24 hours
 	defaultPasswordLinlength = 8
 	defaultetcd              = ""
@@ -43,8 +39,6 @@ const (
 //   LISTEN: either :port or ip:port
 //   MONGODB_URL: mongodb server's url
 //   MONGODB_DATABASE: database's name for mongodb
-//   MONGODB_USERNAME: username for mongodb
-//   MONGODB_PASSWORD: password for mongodb
 //   SESSION_LIFESPAN: validity time limit of sessions (in seconds)
 //   PASSWD_MIN_LENGTH: minimum password length
 //   ETCD: (for future use)
@@ -56,8 +50,6 @@ func Config() Conf {
 	ret.Listen = defaultListen
 	ret.DbURL = defaultDbURL
 	ret.DbName = defaultDbName
-	ret.DbUserName = defaultDbUserName
-	ret.DbPwd = defaultDbPwd
 	ret.SessionLifespan = defaultSessionLifespan
 	ret.PasswordMinLength = defaultPasswordLinlength
 	ret.Etcd = defaultetcd
@@ -66,22 +58,16 @@ func Config() Conf {
 	ret.Root = defaultRoot
 
 	log.Println("port =", os.Getenv("PORT"))
-	log.Println("mongodb_url =", os.Getenv("MONGODB_URL"))
+	log.Println("mongo_url =", os.Getenv("MONGO_URL"))
 	log.Println("mongodb_database =", os.Getenv("MONGODB_DATABASE"))
 	if os.Getenv("PORT") != "" {
 		ret.Listen = ":" + os.Getenv("PORT")
 	}
-	if os.Getenv("MONGODB_URL") != "" {
+	if os.Getenv("MONGO_URL") != "" {
 		ret.DbURL = os.Getenv("MONGODB_URL")
 	}
 	if os.Getenv("MONGODB_DATABASE") != "" {
 		ret.DbName = os.Getenv("MONGODB_DATABASE")
-	}
-	if os.Getenv("MONGODB_USERNAME") != "" {
-		ret.DbUserName = os.Getenv("MONGODB_USERNAME")
-	}
-	if os.Getenv("MONGODB_PASSWORD") != "" {
-		ret.DbPwd = os.Getenv("MONGODB_PASSWORD")
 	}
 	if os.Getenv("SESSION_LIFESPAN") != "" {
 		lifespan, err := strconv.ParseInt(os.Getenv("SESSION_LIFESPAN"), 10, 64)

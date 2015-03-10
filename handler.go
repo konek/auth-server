3e59f90dbca364bcd9a49c2601f9e22532c28e0d
yazou <yazgazan@gmail.com>
2015-03-10 10:54:22 -0400

    ...

--- handler.go
+++ handler.go
@@ -1,69 +1,66 @@
-
 package main
 
 import (
-  "log"
-  "net/http"
-  "encoding/json"
+	"encoding/json"
+	"log"
+	"net/http"
 
-  "github.com/julienschmidt/httprouter"
+	"github.com/julienschmidt/httprouter"
 
-  "github.com/konek/auth-server/controllers"
-  "github.com/konek/auth-server/tools"
-  "github.com/konek/auth-server/config"
+	"github.com/konek/auth-server/config"
+	"github.com/konek/auth-server/controllers"
+	"github.com/konek/auth-server/tools"
 )
 
 func handler(conf config.Conf, fn controllers.ControllerFunc) httprouter.Handle {
-  return func (w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-    resp, err := fn(tools.Handle{
-      R: r,
-      P: p,
-      C: conf,
-    })
-    if err == nil {
-      err = writeJSON(w, resp)
-    }
-    if err != nil {
-      if errDetails, ok := err.(tools.APIError); ok == true {
-        writeError(w, errDetails)
-      } else {
-        log.Printf("Error: %s", err)
-        writeError(w, tools.NewError(
-          err,
-          500,
-          "An unexpected error occured, please contact an administrator"))
-      }
-    }
-  }
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		resp, err := fn(tools.Handle{
+			R: r,
+			P: p,
+			C: conf,
+		})
+		if err == nil {
+			err = writeJSON(w, resp)
+		}
+		if err != nil {
+			if errDetails, ok := err.(tools.APIError); ok == true {
+				writeError(w, errDetails)
+			} else {
+				log.Printf("Error: %s", err)
+				writeError(w, tools.NewError(
+					err,
+					500,
+					"An unexpected error occured, please contact an administrator"))
+			}
+		}
+	}
 }
 
-
 func writeJSON(w http.ResponseWriter, data interface{}) error {
-  chunk, err := json.Marshal(data)
-  if err != nil {
-    return err
-  }
+	chunk, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
 
-  w.Header().Set("Content-Type", "aplication/json")
-  w.WriteHeader(200)
+	w.Header().Set("Content-Type", "aplication/json")
+	w.WriteHeader(200)
 
-  _, err = w.Write(chunk)
-  return err
+	_, err = w.Write(chunk)
+	return err
 }
 
 func writeError(w http.ResponseWriter, e tools.APIError) error {
-  chunk, err := json.Marshal(tools.ErrorResponse{
-    Status: "error",
-    Message: e.Error(),
-  })
-  if err != nil {
-    return err
-  }
+	chunk, err := json.Marshal(tools.ErrorResponse{
+		Status:  "error",
+		Message: e.Error(),
+	})
+	if err != nil {
+		return err
+	}
 
-  w.Header().Set("Content-Type", "aplication/json")
-  w.WriteHeader(e.Code)
+	w.Header().Set("Content-Type", "aplication/json")
+	w.WriteHeader(e.Code)
 
-  _, err = w.Write(chunk)
-  return err
+	_, err = w.Write(chunk)
+	return err
 }
-
--- main.go
+++ main.go
@@ -1,42 +1,42 @@
-
 package main
 
 import (
-  "log"
-  "net/http"
+	"fmt"
+	"log"
+	"net/http"
 
-  "github.com/julienschmidt/httprouter"
+	"github.com/julienschmidt/httprouter"
 
-  c "github.com/konek/auth-server/controllers"
-  "github.com/konek/auth-server/models"
-  "github.com/konek/auth-server/config"
+	"github.com/konek/auth-server/config"
+	c "github.com/konek/auth-server/controllers"
+	"github.com/konek/auth-server/models"
 )
 
 func main() {
-  router := httprouter.New()
-  conf := config.Config()
-  root := conf.Root
+	router := httprouter.New()
+	conf := config.Config()
+	root := conf.Root
 
-  router.POST(root + "/user", handler(conf, c.CreateUser))
-  router.GET(root + "/user/:uid", handler(conf, c.InfosUser))
-  router.PUT(root + "/user/:uid", handler(conf, c.EditUser))
-  router.DELETE(root + "/user/:uid", handler(conf, c.DeleteUser))
+	router.POST(root+"/user", handler(conf, c.CreateUser))
+	router.GET(root+"/user/:uid", handler(conf, c.InfosUser))
+	router.PUT(root+"/user/:uid", handler(conf, c.EditUser))
+	router.DELETE(root+"/user/:uid", handler(conf, c.DeleteUser))
 
-  router.GET(root + "/session/:token", handler(conf, c.InfosSession))
-  router.POST(root + "/session", handler(conf, c.Login))
-  router.PUT(root + "/session", handler(conf, c.Check))
-  router.DELETE(root + "/session/:token", handler(conf, c.Logoff))
+	router.GET(root+"/session/:token", handler(conf, c.InfosSession))
+	router.POST(root+"/session", handler(conf, c.Login))
+	router.PUT(root+"/session", handler(conf, c.Check))
+	router.DELETE(root+"/session/:token", handler(conf, c.Logoff))
 
-  router.POST(root + "/auth", handler(conf, c.Auth))
-  router.GET(root + "/clean/:age", handler(conf, c.Clean))
+	router.POST(root+"/auth", handler(conf, c.Auth))
+	router.GET(root+"/clean/:age", handler(conf, c.Clean))
 
-  router.GET(root + "/list/users", handler(conf, c.ListUsers))
-  router.GET(root + "/list/sessions", handler(conf, c.ListSessions))
+	router.GET(root+"/list/users", handler(conf, c.ListUsers))
+	router.GET(root+"/list/sessions", handler(conf, c.ListSessions))
 
-  err := models.Init(conf)
-  if err != nil {
-    log.Fatal(err)
-  }
-  log.Fatal(http.ListenAndServe(conf.Listen, router))
+	err := models.Init(conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("listening on ", conf.Listen)
+	log.Fatal(http.ListenAndServe(conf.Listen, router))
 }
-
